7c1ac9bbdaac331a7dc976c646b74a6f0a3c2adf
Florian Praile <praile.florian@gmail.com>
2018-09-11 16:45:38 +0200

    paging

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
 
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    var pageViewController: UIPageViewController!
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let blueVc = storyboard.instantiateViewController(withIdentifier: "blue")
+        let redVC = storyboard.instantiateViewController(withIdentifier: "red")
+        let grayVC = storyboard.instantiateViewController(withIdentifier: "gray")
+        return [blueVc, redVC, grayVC]
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pageViewController.dataSource = self
+        pageViewController.delegate = self
+        pageViewController.setViewControllers([orderedViewControllers.first!],
+                                              direction: .forward,
+                                              animated: true,
+                                              completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "embededPaging"{
+            pageViewController = segue.destination as! UIPageViewController
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let next = orderedViewControllers.index(of: viewController)! + 1
+        if next < orderedViewControllers.count{
+            return orderedViewControllers[next]
+        }
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let prev = orderedViewControllers.index(of: viewController)! - 1
+        if prev >= 0{
+            return orderedViewControllers[prev]
+        }
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if !completed { return}
+        if let vc = pageViewController.viewControllers?.first{
+            self.segmentedControl.selectedSegmentIndex = orderedViewControllers.index(of: vc)!
+        }
+        
+    }
+    
+    @IBAction func segmentChange(_ sender: Any) {
+        let newIndex = self.segmentedControl.selectedSegmentIndex
+        if let vc = pageViewController.viewControllers?.first{
+            let currentIndex = orderedViewControllers.index(of: vc)!
+            pageViewController.setViewControllers([orderedViewControllers[newIndex] ],
+                                                  direction: currentIndex < newIndex ? .forward : .reverse ,
+                                                  animated: true,
+                                                  completion: nil)
+        }
+        
+    }
+    
+    
 }
 
